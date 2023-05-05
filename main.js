33e4818efa951802d87dec2b8aba2ec172ad4d00
Efren <efren.alvarez@iteso.mx>
2023-05-05 11:09:16 -0600

    adding main js

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3001;
+
+const authRouter = require('./routes/auth-route.js')
+//const taskRouter = require('./routes/tasks-route.js');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+
+//app.use('/api/tasks', taskRouter)
+app.use('/api/login', authRouter)
+
+app.listen(port, ()=> console.log("Running on port "+ port))
