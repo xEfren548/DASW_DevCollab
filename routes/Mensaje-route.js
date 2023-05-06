6ff3ad4f1334d819aac08062a537ec71c2cae281
ddkktt <128318872+ddkktt@users.noreply.github.com>
2023-05-06 12:34:58 -0600

    Add files via upload

---
+++ routes/Mensaje-route.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const {validarMensaje} = require('../middlewares/validar-datos.js');
+
+const router = express.Router();
+const { Message } = require('../db/Mensaje.js');
+
+router.post('/messages',validarMensaje, async (req, res) => {
+    const { sender, projectId, content } = req.body;
+    
+    const newMessage = await Message.create({ sender, projectId, content });
+
+    res.status(201).send(newMessage);
+});
+
+router.get('/messages/:projectId', async (req, res) => {
+    const projectId = req.params.projectId;
+    const messages = await Message.find({ projectId }).populate('sender');
+    res.send(messages);
+});
+
+router.delete('/messages/:projectId', async (req, res) => {
+    const projectId = req.params.projectId;
+    try {
+      const result = await Message.deleteMessagesByProjectId(projectId);
+      res.send({ message: 'Messages deleted successfully'});
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: 'An error occurred while deleting messages.' });
+    }
+  });
+  
+
+
+module.exports = router;
+
+
